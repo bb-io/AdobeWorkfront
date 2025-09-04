a3d5b7ff979146348b61a7b0fe887fc008437373
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-09-04 16:01:01 +0300

    fix: 🐛 correct parameter naming in `CustomFieldDataHandler`; updated `uploadFileRequest` to `parentRequest` for consistency; improved clarity in error handling for parent type and ID checks

--- Apps.AdobeWorkfront/Handlers/CustomFieldDataHandler.cs
+++ Apps.AdobeWorkfront/Handlers/CustomFieldDataHandler.cs
@@ -7,28 +7,29 @@ using RestSharp;
 
 namespace Apps.AdobeWorkfront.Handlers;
 
-public class CustomFieldDataHandler(InvocationContext invocationContext, [ActionParameter] ParentRequest uploadFileRequest) 
+public class CustomFieldDataHandler(InvocationContext invocationContext, [ActionParameter] ParentRequest parentRequest) 
     : Invocable(invocationContext), IAsyncDataSourceItemHandler
 {
     public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
     {
-        if (string.IsNullOrEmpty(uploadFileRequest.ParentType))
+        if (string.IsNullOrEmpty(parentRequest.ParentType))
         {
             throw new Exception("Parent type must be specified to get parent IDs.");
         }
         
-        if (string.IsNullOrEmpty(uploadFileRequest.ParentId))
+        if (string.IsNullOrEmpty(parentRequest.ParentId))
         {
             throw new Exception("Parent ID must be specified to get custom fields.");
         }
 
-        var apiParentType = uploadFileRequest.GetParentTypeForApi();
-        var requestUrl = $"/attask/api/v19.0/{apiParentType}/{uploadFileRequest.ParentId}";
+        var apiParentType = parentRequest.GetParentTypeForApi();
+        var requestUrl = $"/attask/api/v19.0/{apiParentType}/{parentRequest.ParentId}";
         var apiRequest = new RestRequest(requestUrl)
             .AddQueryParameter("fields", "parameterValues:*");
 
         var response = await Client.ExecuteWithErrorHandling<DataWrapperDto<ObjectWithCustomFieldsDto>>(apiRequest);
         return response.Data.CustomFields
+            .Where(x => x.Value is string)
             .Where(x => context.SearchString == null || x.Key.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
             .Select(cf => new DataSourceItem(cf.Key, cf.Key))
             .ToList();
