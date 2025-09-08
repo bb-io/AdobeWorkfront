52c55d5eaddafae840706a74507234271cbdc973
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-09-08 14:49:30 +0300

    fix: added 202 status code for webhooks responses

--- Apps.AdobeWorkfront/Webhooks/BaseWebhookList.cs
+++ Apps.AdobeWorkfront/Webhooks/BaseWebhookList.cs
@@ -1,4 +1,5 @@
-﻿using Apps.AdobeWorkfront.Webhooks.Payload;
+﻿using System.Net;
+using Apps.AdobeWorkfront.Webhooks.Payload;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Common.Webhooks;
 using Newtonsoft.Json;
@@ -21,19 +22,28 @@ public abstract class BaseWebhookList(InvocationContext invocationContext) : Inv
             throw new ArgumentException("[Workfront] Unable to deserialize incoming webhook body.");
         }
 
+        var authHeader = webhookRequest.Headers.FirstOrDefault(h => h.Key.Equals("Authorization", StringComparison.OrdinalIgnoreCase));
+        var responseMessage = new HttpResponseMessage(HttpStatusCode.Accepted);
+        if (!string.IsNullOrEmpty(authHeader.Value))
+        {
+            responseMessage.Headers.Add("Authorization", authHeader.Value);
+        }
+        
         if (triggerFlight.Invoke(payload) == false)
         {
             return Task.FromResult(new WebhookResponse<T>
             {
                 ReceivedWebhookRequestType = WebhookRequestType.Preflight,
-                Result = payload.NewState
+                Result = payload.NewState,
+                HttpResponseMessage = responseMessage
             });
         }
 
         return Task.FromResult(new WebhookResponse<T>
         {
             ReceivedWebhookRequestType = WebhookRequestType.Default,
-            Result = payload.NewState
+            Result = payload.NewState,
+            HttpResponseMessage = responseMessage
         });
     }
 }
